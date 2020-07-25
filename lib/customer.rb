bbd94f9d49fc13da5ead30f339b9840a1570cfd1
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-25 01:16:31 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -22,5 +22,10 @@ class Customer
     def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
     end
-
+  
+  def waiters
+  meals.map do |meal|
+    meal.waiter
+   end
+  end
 end
