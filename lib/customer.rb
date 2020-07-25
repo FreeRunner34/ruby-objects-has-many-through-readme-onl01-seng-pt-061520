a85f89945c6a839e2a544ee88963f89b4392261f
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-25 01:15:17 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,6 +16,11 @@ class Customer
    def meals
   Meal.all.select do |meal|
     meal.customer == self
-  end
-end
+   end
+  end 
+  
+    def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+    end
+
 end
