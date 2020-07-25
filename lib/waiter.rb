0e1fc4c72f709790b5774200bb645b6be4d1326b
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-25 01:11:20 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -21,13 +21,22 @@ class Waiter
   def waiters
   meals.map do |meal|
     meal.waiter
-  end
+   end
+  end 
   
   def meals
   Meal.all.select do |meal|
     meal.waiter == self #checking for waiter now
-  end
-end
+    end
+  end 
+  
+  def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+    end
+  end 
+ 
+  best_tipped_meal.customer
 end
 end
 
