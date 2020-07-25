2b8fee935792d887f24119f7d647c629d81dc233
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-25 01:08:32 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,27 +1,20 @@
 class Meal
-  attr_accessor :name, :yrs_experience
+ 
+  attr_accessor :waiter, :customer, :total, :tip
  
   @@all = []
  
-  def initialize(name, yrs_experience)
-    @name = name
-    @yrs_experience = yrs_experience
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
     @@all << self
   end
  
   def self.all
     @@all
   end
-  
-   def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
-  end
-  
-  def meals
-  Meal.all.select do |meal|
-    meal.customer == self
-  end
 end
-end 
 
  
