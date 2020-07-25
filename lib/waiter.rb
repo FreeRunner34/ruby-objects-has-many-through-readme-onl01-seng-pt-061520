8077ad36433234f24cbe069a3cfe039735f2ab75
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-25 01:09:57 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,10 +13,9 @@ class Waiter
   def self.all
     @@all
   end
- 
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
   end
-  
 end
 
